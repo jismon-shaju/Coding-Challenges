107be71057842fa26edfc34e35a2aa93f093fc70
jismon-shaju <jismonjomon@gmail.com>
2020-02-17 19:04:28 +0000

    https://www.codewars.com/kata/546e2562b03326a88e000020/train/javascript

---
+++ index.js
@@ -0,0 +1,9 @@
+// Square Every Digit
+// https://www.codewars.com/kata/546e2562b03326a88e000020/train/javascript
+
+function squareDigits(num){
+  var n = num.toString().split('').map(function(x){
+    return Number(x) * Number(x);
+  });
+  return(Number(n.join('')));
+}
